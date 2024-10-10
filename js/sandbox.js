1670031a83c70c18ea5cafb8bae04bedf1632630
Tyler <126TKuncken@frhsd.com>
2024-10-10 12:51:04 -0400

    ARRAYS

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,13 +1,22 @@
-// strings
-console.log('hello, world');
+let ninjas = ['shaun', 'ryu', 'chun-li' ];
 
-// string concatenation
+// ninjas[1] = 'ken'';
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35]
+// console.log(ages[2]);
+
+// console.log(ninjas.length);
 
 
-// getting characters
+// console.log(ninjas.lenght);
+
+// array methods
+
+//let result = ninjas.join('-')
+//let result = ninjas.indexOf('chun-li')
+//let result = ninjas.concat(['ken', 'crystal']);
+let result = ninjas.push('ken');
 
 
-// string length
-
-
-// string methods
+console.log(ninjas);
